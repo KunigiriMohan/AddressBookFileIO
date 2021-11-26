cf0df36cfb82179bdf2f66a13b06b5065061f4b4
KunigiMohan <kmohanr5@gmail.com>
2021-11-26 16:35:40 +0530

    Added : Refactor the code to details to file.

---
+++ src/AddressBook.java
@@ -0,0 +1,16 @@
+/*
+
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        AddressBookMain makeBook = new AddressBookMain();
+        makeBook.main(
+        boolean condition =true;
+        while (condition){
+            boolean monitorUser = makeBook.takeOption();
+            if (monitorUser)
+                break;
+        }
+    }
+}*/
